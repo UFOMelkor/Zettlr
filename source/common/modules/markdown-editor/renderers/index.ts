fe1f81d26738475eef79047652715d7b53459439
Oskar Pfeifer-Bley <oskar@programming-php.net>
2023-05-31 08:12:18 +0200

    Add missing registration for kroki renderer

--- source/common/modules/markdown-editor/renderers/index.ts
+++ source/common/modules/markdown-editor/renderers/index.ts
@@ -65,7 +65,7 @@ const transactionExtender = EditorState.transactionExtender.from(configField, co
  * @return  {Extension}                             The extension set
  */
 export function renderers (config?: Partial<EditorConfiguration>): Extension {
-  const ext: Extension[] = [ renderMermaid, renderAcronyms ]
+  const ext: Extension[] = [ renderMermaid, renderKroki, renderAcronyms ]
   if (config?.renderImages === true) ext.push(renderImages)
   if (config?.renderLinks === true) ext.push(renderLinks)
   if (config?.renderMath === true) ext.push(renderMath)
